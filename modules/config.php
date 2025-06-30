9afbb55c8ec6ae6bdf803b1a478c3834181746da
hamedrepublic <skymobiletm@gmail.com>
2025-06-30 20:36:24 +0330

    Add files via upload

---
+++ modules/config.php
@@ -0,0 +1,26 @@
+<?php
+$sources = [
+    'ProxyMTProto_tel',
+    'GlypeX',
+    'mitivpn',
+    'proxymaann',
+    'mtpro_xyz',
+    'jackalproxy',
+    'Lion_MTProto',
+    'ProxyMTProto',
+    'FarahProxy',
+    'Myporoxy',
+    'irproxy',
+    'Proxy_fier',
+    'TelMTProto',
+    'PlusMTProto',
+    'MTP_roto',
+    'ProxyHagh',
+    'NextGenProxy',
+    'proxyhubs',
+    'ProxyDaemi',
+    'proxyme',
+    'Melli_Config'
+    ];
+
+?>
