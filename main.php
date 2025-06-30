2b96b86535f94b4cfc0474b86d8e2baa3897f263
hamedrepublic <skymobiletm@gmail.com>
2025-07-01 03:07:55 +0330

    Update main.php

--- main.php
+++ main.php
@@ -1,15 +1,15 @@
 <?php
-include_once "modules/config.php";
-include_once "modules/get_proxy.php";
-include_once "modules/ping.php";
-include_once "modules/ipinfo.php";
-include_once "modules/flag.php";
+include_once __DIR__ . "/modules/config.php";
+include_once __DIR__ . "/modules/get_proxy.php";
+include_once __DIR__ . "/modules/ping.php";
+include_once __DIR__ . "/modules/ipinfo.php";
+include_once __DIR__ . "/modules/flag.php";
 
 $final_data = [];
 foreach ($sources as $source) {
     $final_data = array_merge($final_data, proxy_array_maker($source));
-    $final_output = remove_duplicate($final_data); // ← اینم داخل get_proxy.php تعریف شده
+    $final_output = remove_duplicate($final_data);
 }
 
-file_put_contents("proxy/mtproto.json", json_encode($final_output, JSON_PRETTY_PRINT));
+file_put_contents(__DIR__ . "/proxy/mtproto.json", json_encode($final_output, JSON_PRETTY_PRINT));
 ?>
